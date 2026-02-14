797d428bd9c62b680c51fe583ceb8444479c8f0b
mniabedii <abedii.mni@gmail.com>
2026-02-15 02:11:01 +0330

    change preprocessing.py to perform pipeline preprocessing

--- src/preprocessing/preprocessing.py
+++ src/preprocessing/preprocessing.py
@@ -1,54 +1,58 @@
-### Importing the libraries
-import numpy as np
+import os
 import pandas as pd
+import joblib
 
-### Importing the dataset
-dataset = pd.read_csv("Bank_Customers.csv")
-X = dataset.iloc[:, 3:-1].values
-y = dataset.iloc[:, -1].values
-# print(X)
-# print(y)
-
-### Data preprocessing
-
-## Splitting the data into train & test
 from sklearn.model_selection import train_test_split
+from sklearn.compose import ColumnTransformer
+from sklearn.pipeline import Pipeline
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import OneHotEncoder, StandardScaler
+
+dataset = pd.read_csv("../../data/raw/Bank_Customers.csv")
+
+X = dataset.iloc[:, 3:-1]
+y = dataset.iloc[:, -1]
 
 X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=42, stratify=y
 )
 
-## Replace missing data
-from sklearn.impute import SimpleImputer
-
-numerical_columns = [0, 3, 4, 5, 6, 9]
-categorical_columns = [1, 2, 7, 8]
-
-imputer = SimpleImputer(missing_values=np.nan, strategy="median")
-imputer.fit(X_train[:, numerical_columns])
-X_train[:, numerical_columns] = imputer.transform(X_train[:, numerical_columns])
-X_test[:, numerical_columns] = imputer.transform(X_test[:, numerical_columns])
-
-imputer = SimpleImputer(missing_values=np.nan, strategy="most_frequent")
-imputer.fit(X_train[:, categorical_columns])
-X_train[:, categorical_columns] = imputer.transform(X_train[:, categorical_columns])
-X_test[:, categorical_columns] = imputer.transform(X_test[:, categorical_columns])
-
-## One-hot encoding the geography & gender column
-from sklearn.compose import ColumnTransformer
-from sklearn.preprocessing import OneHotEncoder
-
-ct = ColumnTransformer(
-    transformers=[("encoder", OneHotEncoder(sparse_output=False), [1, 2])],
-    remainder="passthrough",
+# Build preprocessing
+preprocess = ColumnTransformer(
+    transformers=[
+        (
+            "num",
+            Pipeline(
+                [
+                    ("imputer", SimpleImputer(strategy="median")),
+                    ("scaler", StandardScaler()),
+                ]
+            ),
+            [
+                "CreditScore",
+                "Age",
+                "Tenure",
+                "Balance",
+                "NumOfProducts",
+                "HasCrCard",
+                "IsActiveMember",
+                "EstimatedSalary",
+            ],
+        ),
+        (
+            "cat",
+            Pipeline(
+                [
+                    ("imputer", SimpleImputer(strategy="most_frequent")),
+                    (
+                        "onehot",
+                        OneHotEncoder(handle_unknown="ignore", sparse_output=False),
+                    ),
+                ]
+            ),
+            ["Geography", "Gender"],
+        ),
+    ]
 )
-X_train = np.array(ct.fit_transform(X_train))
-X_test = np.array(ct.transform(X_test))
 
-## Feature Scaling
-from sklearn.preprocessing import StandardScaler
-
-sc = StandardScaler()
-X_train = sc.fit_transform(X_train)
-X_test = sc.transform(X_test)
-print(X_train)
+preprocess.fit(X_train)
